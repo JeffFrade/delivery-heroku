ba21de82eeb97c6a4899b1fea7df682d56f10141
Jefferson Frade <jefferrson.frade@gmail.com>
2018-07-18 15:17:01 -0300

    Alter title

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -2,13 +2,13 @@
 <html lang="pt-br">
     <head>
         <meta charset="utf-8"/>
-        <title>Codecasts - Laravel Básico</title>
+        <title>Laravel Básico</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     </head>
 
     <body>
         <div class="container">
-            <h1>Bem vindo ao mundo Laravel</h1>
+            <h1>Clientes</h1>
 
             <table class="table table-bordered table-striped">
                 <tr>
